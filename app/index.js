2aebd1313490ec0e68d95b6cac8acf82b0f1f8a9
ppawelll <p.pszonka@gmail.com>
2016-06-07 09:51:54 +0000

    init commit

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
+import angular from 'angular';
+import uirouter from 'angular-ui-router';
+
+import routing from './config';
+
+const ngModule = angular
+                    .module('app', [uirouter])
+                    .config(routing);
